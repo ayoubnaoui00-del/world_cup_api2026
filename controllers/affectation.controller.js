bd935960dd5bafd91e09a4df5ad926268b2cea14
ayoubnaoui00-del <ayoubnaoui00@gmail.com>
2026-07-10 13:00:53 +0200

    added include

--- controllers/affectation.controller.js
+++ controllers/affectation.controller.js
@@ -2,6 +2,7 @@ import Affectation from '../models/affectation.model.js';
 import Referee from '../models/arbitre.model.js';
 import Match from '../models/matches.model.js';
 
+
 export const createAffectation = async (req, res) => {
   try {
     const { refereeId, matchId, role } = req.body;
@@ -48,39 +49,54 @@ export const createAffectation = async (req, res) => {
 
 export const getAllAffectation = async (req, res) => {
   try {
-    const affectation = await Affectation.findAll();
-    res.status(200).json({ message: "Assignment retrieved successfully", data: affectation })
+    const affectation = await Affectation.findAll({
+      include: [
+        {
+          model: Referee,
+         
+          attributes: ['firstName', 'lastName', 'nationality', 'confederation', 'category', 'experience', 'status'],
+        },
+        {
+          model: Match,
+          
+          attributes: ['id', 'homeTeam', 'awayTeam', 'stadium', 'hostCity', 'matchDate', 'phase'],
+        },
+      ],
+    });
+
+    res.status(200).json({ message: 'Assignment retrieved successfully', data: affectation });
   } catch (error) {
-    res.status(500).json({ error: 'server error' })
+    res.status(500).json({ error: 'server error' });
   }
-}
+};
 
 export const getById = async (req, res) => {
   try {
-    const { id } = req.params.id;
-    if (!id) return res.status(404).json("data not found")
+    const { id } = req.params;
+    if (!id) return res.status(404).json({ error: 'data not found' });
     const data = await Affectation.findByPk(id);
-    if (!data) return res.status(404).json("data not found")
+    if (!data) return res.status(404).json({ error: 'data not found' });
+    res.status(200).json({ message: 'Affectation retrieved successfully', data });
   } catch (error) {
-    res.status(500).json({ error: "server error" })
+    res.status(500).json({ error: 'server error' });
   }
-}
+};
 
 export const updateAffectation = async (req, res) => {
-
   try {
     const { id } = req.params;
-    const affectation = await Affectation.findByPk(ud);
+    const affectation = await Affectation.findByPk(id);
     if (!affectation) {
-      returnres.status(404).json({
-        error: 'Affectation not found'
+      return res.status(404).json({
+        error: 'Affectation not found',
       });
     }
-    await affectation.update(req.body)
+    await affectation.update(req.body);
+    res.status(200).json({ message: 'Affectation updated successfully', data: affectation });
   } catch (error) {
-    res.status(500).json({ error: "server error" });
+    res.status(500).json({ error: 'server error' });
   }
-}
+};
 export const deleteAffectation = async (req, res) => {
   try {
     const { id } = req.params;
